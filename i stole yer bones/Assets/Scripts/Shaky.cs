e7a16aa71a53dd07785c3b58998d504547acd04a
Nick Rasmussen <nickras35@gmail.com>
2020-02-01 22:11:37 -0500

    shake it, shake shake it, shake it, shake shake it

---
+++ i stole yer bones/Assets/Scripts/Shaky.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaky : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartShake(float intensity, float duration)
+    {
+        StartCoroutine(Shake(intensity, duration));
+    }
+
+    IEnumerator Shake(float intensity, float duration)
+    {
+        float time = 0;
+        Vector3 origin = transform.position;
+        Vector3 newPos = transform.position;
+        while(time < duration)
+        {
+            time += Time.deltaTime;
+            newPos.x += Random.Range(-intensity, intensity);
+            newPos.y += Random.Range(-intensity, intensity);
+            transform.position = newPos;
+            yield return null;
+        }
+        transform.position = origin;
+        yield return null;
+    }
+}
